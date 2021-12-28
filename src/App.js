86d830a8efb4f5f26bf9760be736ae15a85b23ac
David Moina <davemolina777@gmail.com>
2021-12-28 08:33:01 -0600

    Import Css file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
+    <div className='app-container'>
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Address</th>
-          <th>Phone Number</th>
-          <th>Email</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
         <tbody>
           <tr>
-            <td></td>
+            <td>Jenny Chan</td>
+            <td>3 waterfoot road</td>
+            <td>123-424-0987</td>
           </tr>
         </tbody>
       </table>
